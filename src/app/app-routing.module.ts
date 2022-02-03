66f501ca98927e06dbef6ae089912928a12e9016
Stian Økland <stian.okland@no.experis.com>
2022-02-03 14:49:41 +0100

    FEAT: Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LandingComponent } from './landing/landing.component';
+import { TrainerComponent } from './trainer/trainer.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+
+const routes: Routes = [
+  {path: '', redirectTo:'/landing', pathMatch: 'full'},
+
+  {path: 'landing', component: LandingComponent},
+  {path: 'trainer', component: TrainerComponent},
+  {path: 'catalogue', component: CatalogueComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { TrainerComponent } from './trainer/trainer.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    TrainerComponent,
+    CatalogueComponent
   ],
   imports: [
     BrowserModule,
