515455e4b310d53f1325b78f5e9d60cedfa1661d
Andrea Nakstad <andrea.nakstad@yahoo.com>
2022-02-04 14:31:04 +0100

    feat: basic login page and user-api service added

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { TrainerType } from '../models/trainer.model';
+import { UserApiService } from '../services/user-api.service';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent implements OnInit {
+export class LandingComponent {
+  currentUser: TrainerType | undefined = undefined
 
-  constructor() { }
+  constructor(private readonly userAPIService: UserApiService) { }
 
-  ngOnInit(): void {
+  attemptLogin(username: string){
+    // checks if user exists, otherwise registers a new user
+
+    this.userAPIService.fetchUserGivenUsername(username)
+    .subscribe(
+      (trainers) => {
+        if (trainers[0]){
+          // user exists 
+
+          //TODO: store in localStorage & redirect
+          this.currentUser = trainers[0]
+        }
+        else {
+          // user does not exist -> register new user
+          this.userAPIService.registerNewUser(username)
+          .subscribe(
+            (newUser) => {
+              //TODO: store in localStorage & redirect
+              this.currentUser = newUser as TrainerType
+            },
+            (error) => {
+              console.error(error.message)
+            }
+          )
+        }
+      },
+      (error) => {
+        console.error(error.message)
+      }
+    )
   }
-
 }
---
+++ src/app/models/trainer.model.ts
@@ -0,0 +1,5 @@
+export type TrainerType = {
+    id: number;
+    name: string;
+    pokemon: string[];
+}
